e86bc62a83dae29fadf5bef2785112ab3e24c765
wushichen <1423449690@qq.com>
2024-02-21 00:03:51 +0800

    修改了A组件中的跳跳

--- src/components/A.vue
+++ src/components/A.vue
@@ -1,10 +1,12 @@
 <script setup>
+    import {ref} from 'vue'
 
+    const msg = ref('我是A组件中的跳跳')
 </script>
 
 <template>
     <div>
-        我是A组件
+        我是A组件 -- {{ msg }}
     </div>
 </template>
 
