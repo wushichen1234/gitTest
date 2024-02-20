94df01a0c348c355fc4f18cfa0a7fc54ead9cced
wushichen <1423449690@qq.com>
2024-02-21 00:02:14 +0800

    修改了A组件中的哈哈

--- src/components/A.vue
+++ src/components/A.vue
@@ -1,10 +1,13 @@
 <script setup>
+    import {ref} from 'vue'
+
+    const msg = ref('我是A组件中的哈哈')
 
 </script>
 
 <template>
     <div>
-        我是A组件
+        我是A组件 -- {{ msg }}
     </div>
 </template>
 
