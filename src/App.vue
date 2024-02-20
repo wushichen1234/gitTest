9941424ad13a8183af4c5459692f42ba8cc1f351
wushichen <1423449690@qq.com>
2024-02-20 23:56:50 +0800

    提交了A和B组件

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
     import A from '@/components/A.vue'
+    import B from '@/components/B.vue'
 </script>
 
 <template>
     <div>
         <A></A>
+        <B></B>
     </div>
 </template>
 
---
+++ src/components/B.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        我是B组件
+    </div>
+</template>
+
+<style scoped>
+
+</style>
