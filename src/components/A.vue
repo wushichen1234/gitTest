08ea821bc3a86423a866daebb81bd962b5176167
wushichen <1423449690@qq.com>
2024-02-22 21:57:18 +0800

    增加了打电话的按钮

--- src/components/A.vue
+++ src/components/A.vue
@@ -5,11 +5,18 @@
         name: 'wax'
     })
 
+    const call = () => {
+        window.location.href = 'tel:' + 18101930232;
+    }
+
 </script>
 
 <template>
     <div>
         我是A组件 -- {{ person.name }}
+
+        <button @click="call">点我打电话</button>
+
     </div>
 </template>
 
